8bd93659efcf4f461ce1d014428b418d9c414cfa
chromicon <sholto.maud@gmail.com>
2015-04-15 15:35:16 +1000

    sites

--- hydstra/cleanSiteList.js
+++ hydstra/cleanSiteList.js
@@ -5,11 +5,11 @@ module.exports = function (){
   return through(function write(buffer, _, next) {
     var ret;
     var line = buffer.toString().replace(/;$/g,"");
-    console.log('line [',line,']');
     // return key not consistent from Hydstra webservice between agencies!!!
     // It's an outrage sir!!!
     for (objKey in line){
       if (!line.hasOwnProperty(objKey)){ continue; }
+      console.log('objKey [',objKey,']');
       switch (objKey){
         case 'return':
          ret = 'return';
@@ -21,6 +21,7 @@ module.exports = function (){
     }
     
     retrn = line[ret];
+    console.log('line [',retrn,']');
     next();
   },
   function end(cb){
